1e689cd64eaff8c1269a7c791bc5c3a7e8d53854
Fan82 <congeefan82@gmail.com>
2024-05-05 16:00:42 +0800

    up

--- webpp/js/script.js
+++ webpp/js/script.js
@@ -1,52 +1,73 @@
 $(document).ready(function () {
-  $(".load-backdrop").show();
-  setTimeout(function () {
-    $(".load-backdrop").fadeOut();
-    screenWidthCheck();
-    gameHall_JS();
-  }, 4000);
+  gameHall_JS();
+  screenWidthCheck();
+  scrollProject();
 
 });
-function screenWidthCheck() {
-  var screenWidth = window.innerWidth || document.documentElement.clientWidth;
 
-  if (screenWidth < 1000) {
-    $("body").addClass("mobile");
 
-    if ($("body").hasClass("main-wireframe")) {
-      $(".container-side-btn").show();
-    }
+// function screenWidthCheck() {
+//   var screenWidth = window.innerWidth || document.documentElement.clientWidth;
 
-    $(".container-side-btn").click(function () {
-      $(".container-side").toggleClass("show");
-    });
-  } else {
-    $(".container-side-btn").hide();
-  }
-}
+// if (screenWidth < 1000) {
+//   if ($("body").hasClass("main-wireframe")) {
+//     $(".container-side-btn").show();
+//   }
+
+//   $(".container-side-btn").click(function () {
+//     $(".container-side").toggleClass("show");
+//   });
+// } else {
+//   $(".container-side-btn").hide();
+// }
+// }
 
 function gameHall_JS() {
-  $(".container-side-btn").hide();
-
   $("a.contact").click(function () {
     $(".contact-menu").toggleClass('open');
   });
 
-  $("a.btn-tab.Flow").click(function () {
+  $("a.btn-tab.Screen").click(function () {
+    $(".container-side").hide();
+    $(this).toggleClass("active");
     $(".main").addClass("main-project").removeClass("main-wireframe");
+    $(".project-wrapper .container.grid").show();
   });
 
   $("a.btn-tab.Wireframe").click(function () {
-    $(".main").addClass("main-wireframe").removeClass("main-project");
+    $(".container-side").hide();
+    $("body").addClass("main-wireframe").removeClass("main-project");
+    $(this).addClass("active");
+    $("a.btn-tab.Screen").removeClass("active");
+    $(".project-wrapper .container.grid").show();
     $(".project").each(function () {
       if ($(this).find("img.wireframe").length === 0) {
         $(this).css("display", "none");
       }
     });
   });
-
-  $(".mobile nav").click(function () {
+  $("a.btn-tab.Flow").click(function () {
     $(this).toggleClass("active");
+    $("body").removeClass("main-project");
+    $("body").removeClass("main-wireframe");
+    $(".container-side").show();
+    $(".project-wrapper .container.grid").hide();
   });
+}
+function scrollProject() {
+  $(window).scroll(function () {
+    var scrollPosition = $(this).scrollTop();
+    var projectOffset = $('#section-project').offset().top;
+    var targetOffset = projectOffset - 400;
 
+    // 迭代所有 project-img 元素，对它们进行操作
+    $(".project-img img").each(function () {
+      var imgOffset = $(this).offset().top;
+      if (scrollPosition >= targetOffset) {
+        $(this).css("transform", "translateY(-60px)");
+      } else {
+        $(this).css("transform", "translateY(0)");
+      }
+    });
+  });
 }
